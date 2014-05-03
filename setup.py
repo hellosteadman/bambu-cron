12b2ff66d581bd0d8bd08a9061040b4626ccb34e
Mark Steadman <marksteadman@me.com>
2014-05-03 17:32:27 +0100

    Updated package's long description

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author = 'Steadman',
     author_email = 'mark@steadman.io',
     url = 'https://github.com/iamsteadman/bambu-cron',
+    long_description = open(path.join(path.dirname(__file__), 'README')).read(),
     install_requires = ['Django>=1.4'],
     packages = [
         'bambu_cron',
